b2226cc330b64dc32bb6620c9ce1601507379cad
Kenny Pflug <kenny.pflug@live.de>
2021-05-26 07:56:41 +0200

    added additional XML comments to IAsyncReadOnlySession

--- Synnotech.DatabaseAbstractions/IAsyncReadOnlySession.cs
+++ Synnotech.DatabaseAbstractions/IAsyncReadOnlySession.cs
@@ -4,9 +4,14 @@ namespace Synnotech.DatabaseAbstractions
 {
     /// <summary>
     /// Represents an asynchronous session to the database that
-    /// will only be used to read data. The connection to the database
+    /// will only be used to read data. This usually means that no
+    /// transaction is needed for this session. The connection to the database
     /// can be terminated by calling <see cref="IAsyncDisposable.DisposeAsync"/>
     /// (or <see cref="IDisposable.Dispose"/>).
     /// </summary>
+    /// <remarks>
+    /// Conceptually, a session is identical to a "Unit of Work".
+    /// The term "session" is just simpler to use in daily life.
+    /// </remarks>
     public interface IAsyncReadOnlySession : IDisposable, IAsyncDisposable { }
 }
